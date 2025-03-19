63282ebb3e95f345cdbd4588130a4588ae58e957
rendrazuriansyah <rendra.zurd04@gmail.com>
2025-03-19 15:12:35 +0700

    feat(posts): menambahkan store method untuk membuat postingan baru

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -15,5 +16,35 @@ class PostController extends Controller
             'success' => true,
             'data' => $posts,
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'content' => 'required|string|max:255',
+            'image_url' => 'nullable',
+        ]);
+
+        // Jika validasi gagal, kirimkan pesan error
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors(),
+            ], 400);
+        }
+
+        // Jika validasi berhasil, simpan data ke database
+        $post = Post::create([
+            'user_id' => $request->user_id,
+            'content' => $request->content,
+            'image_url' => $request->image_url,
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Berhasil membuat post baru',
+            'data' => $post,
+        ], 201);
     }
 }
