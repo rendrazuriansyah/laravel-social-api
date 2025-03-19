fa8c7b982dac8b6bd001736807f4f59cbef895cf
rendrazuriansyah <rendra.zurd04@gmail.com>
2025-03-19 12:50:37 +0700

    feat(posts-api): menambahkan index untuk ambil semua postingan

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// API Versioning (v1)
+Route::prefix('v1')->group(function () {
+    // Menghandle posts
+    Route::prefix('posts')->group(function () { // http://localhost:8000/api/v1/posts
+        Route::get('/', [PostController::class, 'index']); // Mengambil semua data.
+        Route::post('/', [PostController::class, 'store']); // Menyimpan data.
+        Route::get('{id}', [PostController::class, 'show']); // Mengambil detail data by id.
+        Route::put('{id}', [PostController::class, 'update']); // Mengupdate data by id.
+        Route::delete('{id}', [PostController::class, 'destroy']); // Menghapus data by id.
+    });
+});
