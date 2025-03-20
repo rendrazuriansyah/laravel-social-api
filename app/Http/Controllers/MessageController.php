2cff2b66d20a199d6aeb6d53b0af83e220e47542
rendrazuriansyah <rendra.zurd04@gmail.com>
2025-03-20 23:59:08 +0700

    feat(messages): menambahkan show method untuk mendapatkan pesan berdasarkan id

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -35,4 +35,15 @@ class MessageController extends Controller
             'data' => $message,
         ]);
     }
+
+    public function show(int $id)
+    {
+        $message = Message::find($id);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Berhasil mengambil pesan',
+            'data' => $message,
+        ]);
+    }
 }
