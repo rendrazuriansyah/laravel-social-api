f55374c15d3e6349dbf20eaf5eb178deb1ee0246
rendrazuriansyah <rendra.zurd04@gmail.com>
2025-03-21 00:18:58 +0700

    feat(messages): menambahkan destroy method untuk menghapus pesan berdasarkan id

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -46,4 +46,14 @@ class MessageController extends Controller
             'data' => $message,
         ]);
     }
+
+    public function destroy(int $id)
+    {
+        Message::destroy($id);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Berhasil menghapus pesan',
+        ]);
+    }
 }
